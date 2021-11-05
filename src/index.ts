1e8a25488becacca4ef490a1ce1bd2fe21d24f19
Jay Wang <jay@zijie.wang>
2021-11-05 14:09:41 -0400

    Set up eslint check for ts
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,41 @@
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-import { ICommandPalette, MainAreaWidget, ToolbarButton } from '@jupyterlab/apputils';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import {
+  ICommandPalette,
+  MainAreaWidget,
+  ToolbarButton
+} from '@jupyterlab/apputils';
 import { Widget } from '@lumino/widgets';
 import { Message } from '@lumino/messaging';
 import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 import { DocumentRegistry } from '@jupyterlab/docregistry';
-import { NotebookActions, NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+import {
+  NotebookActions,
+  NotebookPanel,
+  INotebookModel
+} from '@jupyterlab/notebook';
 import { toArray } from '@lumino/algorithm';
 
-export class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
-  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
     /**
      * Handler for the click event.
      */
     const onClickHandler = () => {
-      console.log('button clicked');
+      console.log('button clicked, yo!');
       console.log(panel);
       console.log(context);
 
       // Check if we have already created stickyland
-      let stickyContainer: HTMLElement | null = document.querySelector('.sticky-container');
+      let stickyContainer: HTMLElement | null =
+        document.querySelector('.sticky-container');
 
       // Create it if we don't have it yet
       if (stickyContainer === null) {
@@ -37,14 +53,14 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
       }
 
       // Check if we should show or hide this container
-      if (stickyContainer!.classList.contains('hidden')) {
-        stickyContainer!.classList.remove('hidden');
+      if (stickyContainer?.classList.contains('hidden')) {
+        stickyContainer?.classList.remove('hidden');
       } else {
-        stickyContainer!.classList.add('hidden');
+        stickyContainer?.classList.add('hidden');
       }
     };
 
-    let button = new ToolbarButton({
+    const button = new ToolbarButton({
       className: 'sticky-button',
       iconClass: 'far fa-sticky-note',
       onClick: onClickHandler,
@@ -70,6 +86,5 @@ const plugin = {
     app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
   }
 };
-
 
 export default plugin;
