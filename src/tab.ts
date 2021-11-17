26712f3da87b3c67b9d332cf38d7a8df613493ae
Jay Wang <jay@zijie.wang>
2021-11-17 01:42:37 -0500

    Support swapping tabs
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- src/tab.ts
+++ src/tab.ts
@@ -46,7 +46,13 @@ export class StickyTab implements IDisposable {
     this.node.appendChild(this.addButton);
     MyIcons.addIcon2.element({ container: this.addButton });
 
-    // this.addButton.addEventListener('click', this.clickAddTab);
+    this.addButton.addEventListener('click', (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      // Create a new tab and switch the active tab
+      this.createTab();
+    });
 
     // Create the first tab
     this.createTab();
@@ -61,7 +67,8 @@ export class StickyTab implements IDisposable {
   createTab = (): Tab => {
     // Create a new tab node
     const tabNode = document.createElement('button');
-    tabNode.classList.add('tab');
+    tabNode.classList.add('tab', 'new-tab');
+    tabNode.setAttribute('title', 'New tab');
 
     // Add a label to the button
     const tabLabel = document.createElement('span');
@@ -83,7 +90,10 @@ export class StickyTab implements IDisposable {
     );
 
     // Handle delete icon clicked
-    tabIcon.addEventListener('click', (event: MouseEvent) => {
+    tabIcon.addEventListener('click', (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
       // Case 1: if there are other tabs
 
       // Case 2: this tab is the only tab
@@ -106,18 +116,39 @@ export class StickyTab implements IDisposable {
 
     this.tabs.push(newTab);
     this.node.insertBefore(newTab.tabNode, this.addButton);
-    this.activeTab = newTab;
+
+    // Handle tab clicked
+    tabNode.addEventListener('click', (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      // Switch the active tab to the current one
+      if (this.activeTab?.tabNode !== tabNode) {
+        this.switchActiveTab(newTab);
+      }
+    });
 
     // Move the current active tab to this new one
-    if (this.activeTab) {
-      this.activeTab.tabNode.classList.remove('current');
-    }
-    this.activeTab = newTab;
-    newTab.tabNode.classList.add('current');
-    this.stickyLand.stickyContent = newTab.tabContent;
+    this.switchActiveTab(newTab);
 
     // Return this tab
     return newTab;
+  };
+
+  /**
+   * Change the currently active tab to the new tab
+   */
+  switchActiveTab = (newTab: Tab) => {
+    if (this.activeTab) {
+      // Hide the old active tab's content
+      this.activeTab.tabContent.wrapperNode.classList.add('no-display');
+      this.activeTab.tabNode.classList.remove('current');
+    }
+
+    this.activeTab = newTab;
+    newTab.tabNode.classList.add('current');
+    newTab.tabContent.wrapperNode.classList.remove('no-display');
+    this.stickyLand.stickyContent = newTab.tabContent;
   };
 
   /**
@@ -147,12 +178,35 @@ export class StickyTab implements IDisposable {
       // Update the tab name
       const tabLabel = this.activeTab.tabNode.querySelector('.tab-label');
 
-      if (newCellType === ContentType.Code && tabLabel) {
-        tabLabel.innerHTML = `Code-${newCellIndex}`;
-      } else if (newCellType === ContentType.Markdown && tabLabel) {
-        tabLabel.innerHTML = `Markdown-${newCellIndex}`;
-      } else if (newCellType === ContentType.Dropzone && tabLabel) {
-        tabLabel.innerHTML = 'New';
+      if (tabLabel) {
+        switch (newCellType) {
+          case ContentType.Code:
+            tabLabel.innerHTML = `Code-${newCellIndex}`;
+            this.activeTab.tabNode.setAttribute(
+              'title',
+              `Code-${newCellIndex}`
+            );
+            this.activeTab.tabNode.classList.remove('new-tab');
+            break;
+
+          case ContentType.Markdown:
+            tabLabel.innerHTML = `Markdown-${newCellIndex}`;
+            this.activeTab.tabNode.setAttribute(
+              'title',
+              `Markdown-${newCellIndex}`
+            );
+            this.activeTab.tabNode.classList.remove('new-tab');
+            break;
+
+          case ContentType.Dropzone:
+            tabLabel.innerHTML = 'New';
+            this.activeTab.tabNode.setAttribute('title', 'New tab');
+            this.activeTab.tabNode.classList.add('new-tab');
+            break;
+
+          default:
+            break;
+        }
       }
 
       // Update the model data
@@ -160,92 +214,6 @@ export class StickyTab implements IDisposable {
       this.activeTab.cellType = newCellType;
     }
   };
-
-  // clickTab = (evt: Event) => {
-  //   if (
-  //     (evt.target as Element).getAttribute('class') &&
-  //     (evt.target as Element).className.includes('tab')
-  //   ) {
-  //     const tabcontent = document.getElementsByClassName('sticky-content');
-  //     for (let i = 0; i < tabcontent.length; i++) {
-  //       (tabcontent[i] as HTMLElement).style.display = 'none';
-  //     }
-
-  //     const tabs = document.getElementsByClassName('tab');
-  //     for (let i = 0; i < tabs.length; i++) {
-  //       tabs[i].className = tabs[i].className.replace(' current', '');
-  //     }
-
-  //     const id = (evt.target as Element).getAttribute('name');
-  //     document.getElementById(id!)!.style.display = 'flex';
-
-  //     (evt.target as Element).className += ' current';
-  //   } else {
-  //     const deleteTab = document.getElementsByClassName('delete-tab');
-  //     for (let i = 0; i < deleteTab.length; i++) {
-  //       deleteTab[i].addEventListener('click', this.clickDeleteTab);
-  //     }
-  //   }
-  // };
-
-  // clickAddTab = () => {
-  //   const tabs = document.getElementsByClassName('tab');
-  //   console.log(tabs.length);
-  //   for (let i = 0; i < tabs.length; i++) {
-  //     tabs[i].className = tabs[i].className.replace(' current', '');
-  //   }
-
-  //   const addCell = this.node.lastChild;
-  //   this.node.removeChild(addCell!);
-  //   const cellNum = Dropzone.numDz;
-  //   this.node.innerHTML += `<button class="tab current" name="New ${cellNum}">New ${cellNum}</button>`;
-  //   this.node.appendChild(addCell!);
-
-  //   for (let i = 0; i < tabs.length; i++) {
-  //     tabs[i].addEventListener('click', this.clickTab);
-  //   }
-  //   tabs[StickyTab.numTabs].innerHTML +=
-  //     '<svg class="delete-tab" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" data-icon="ui-components:close" data-icon-id="58648a58-146c-4974-9873-7d2dfb468b8d"><g class="x-icon-circle" fill="none"><circle cx="12" cy="12" r="11"></circle></g><g class="x-icon" fill="#616161"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></g></svg>';
-  //   const deleteTab = document.getElementsByClassName('delete-tab');
-  //   for (let i = 0; i < deleteTab.length; i++) {
-  //     deleteTab[i].addEventListener('click', this.clickDeleteTab);
-  //   }
-
-  //   new StickyContent(this.stickyContainer, this.notebook, this.stickyLand);
-
-  //   const tabcontent = document.getElementsByClassName('sticky-content');
-  //   for (let i = 0; i < tabcontent.length; i++) {
-  //     (tabcontent[i] as HTMLElement).style.display = 'none';
-  //   }
-  //   document.getElementById(`New ${cellNum}`)!.style.display = 'flex';
-  // };
-
-  // clickDeleteTab = (evt: Event) => {
-  //   const deleteTab = (evt.currentTarget as Element).parentNode;
-  //   const deleteHTML = (evt.currentTarget as Element).parentElement;
-  //   let deletedCurrent = false;
-  //   if (deleteHTML!.className.includes('current')) {
-  //     deletedCurrent = true;
-  //   }
-  //   const nameId = deleteHTML!.getAttribute('name');
-  //   const deleteContent = document.getElementById(nameId!);
-  //   this.node.removeChild(deleteTab!);
-  //   this.stickyContainer.removeChild(deleteContent!);
-
-  //   StickyTab.numTabs--;
-  //   if (StickyTab.numTabs > 0) {
-  //     if (deletedCurrent) {
-  //       const tabs = document.getElementsByClassName('tab');
-  //       if (!tabs[0].className.includes('current')) {
-  //         tabs[0].className += ' current';
-  //       }
-  //       document.getElementById(tabs[0].getAttribute('name')!)!.style.display =
-  //         'flex';
-  //     }
-  //   } else {
-  //     this.clickAddTab();
-  //   }
-  // };
 
   dispose = () => {
     this.isDisposed = true;
