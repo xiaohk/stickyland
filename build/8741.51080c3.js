95292febf6a718b57059631173fb3682192167e7
Jay Wang <jay@zijie.wang>
2022-03-07 18:29:48 -0500

    7e149a5dce964bd65a42886ece935343b1e50ecd

---
+++ build/8741.51080c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_JUPYTERLAB_CORE_OUTPUT=self.webpackChunk_JUPYTERLAB_CORE_OUTPUT||[]).push([[8741],{88741:()=>{}}]);
---
+++ build/9866.1500e45.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_JUPYTERLAB_CORE_OUTPUT=self.webpackChunk_JUPYTERLAB_CORE_OUTPUT||[]).push([[9866],{89866:(t,a,e)=>{e.r(a),e.d(a,{default:()=>T});var o=e(14375),r=e(95050),s=e(56615);const T={id:"@jupyterlab/celltags",autoStart:!0,requires:[o.INotebookTools,o.INotebookTracker,s.ITranslator],activate:(t,a,e,o)=>{const s=new r.TagTool(e,t,o);a.addItem({tool:s,rank:1.6})}}}}]);
+//# sourceMappingURL=9866.1500e45.js.map
