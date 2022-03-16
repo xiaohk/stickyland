c8800410631a1a9abcb6b4a36872a820469f0505
Jay Wang <jay@zijie.wang>
2022-03-16 11:05:16 -0400

    abb75fc8a82b6a504296f7c557c87891b6b0ad41

--- build/3863.dd3cd05.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_JUPYTERLAB_CORE_OUTPUT=self.webpackChunk_JUPYTERLAB_CORE_OUTPUT||[]).push([[3863],{73863:(e,t,a)=>{a.r(t),a.d(t,{default:()=>r});var n=a(88706);const r=[{id:"@jupyterlab/ui-components-extension:labicon-manager",provides:n.ILabIconManager,autoStart:!0,activate:e=>Object.create(null)},{id:"@jupyterlab/settingeditor-extension:form-registry",provides:n.IFormComponentRegistry,autoStart:!0,activate:e=>new n.FormComponentRegistry}]}}]);
-//# sourceMappingURL=3863.dd3cd05.js.map
---
+++ build/9866.70a44a7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_JUPYTERLAB_CORE_OUTPUT=self.webpackChunk_JUPYTERLAB_CORE_OUTPUT||[]).push([[9866],{89866:(t,a,e)=>{e.r(a),e.d(a,{default:()=>T});var o=e(2182),r=e(1172),s=e(92541);const T={id:"@jupyterlab/celltags",autoStart:!0,requires:[o.INotebookTools,o.INotebookTracker,s.ITranslator],activate:(t,a,e,o)=>{const s=new r.TagTool(e,t,o);a.addItem({tool:s,rank:1.6})}}}}]);
+//# sourceMappingURL=9866.70a44a7.js.map
